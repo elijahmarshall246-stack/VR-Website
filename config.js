d5a45d341b8665b393eb376584de624538cf65ea
elijahmarshall246-stack <elijahmarshall246@gmail.com>
2026-08-10 10:24:18 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,48 @@
+window.VR_CONFIG = {
+
+  live: {
+    sheetId: '1KmJvICKMVe44DewudUdgdW9AhC_Rif83u6CwELpvZls',
+
+    classFilter: {
+      enabled: true,
+      gid: '29911604',
+      range: '',
+      driverColumn: 'D',
+      classColumn:  'A',
+      allLabel: 'All Classes',
+    },
+
+    events: {
+      sheetId: '1ghHvvVe4kNnkVUbYTsBHxrfB2MLH-erhOCvlOBbU3Uc',
+      gid:     '1914616841',
+    },
+  },
+
+  previous: {
+    workbookId: '1MS1c8w8nRo-5hVPjl-IQRr9UMLNmDj8drFTpq-i3J7w',
+    indexTabName: 'Index',
+    indexColumns: {
+      eventName: 'EventName',
+      eventType: 'EventType',
+      date:      'Date',
+      tabName:   'TabName',
+      pdfUrl:    'PdfUrl',
+    },
+  },
+
+  weather: {
+    lat: 13.17,
+    lon: -59.58,
+    timezone: 'America/Barbados',
+    unit: 'celsius',          // 'celsius' | 'fahrenheit'
+    label1: 'VAUCLUSE RACEWAY',
+    label2: 'CURRENT WEATHER',
+  },
+
+  links: {
+    mainSite:  'https://vaucluseraceway.com',
+    instagram: 'https://instagram.com/vaucluse_raceway',
+    whatsapp:  'https://www.whatsapp.com/channel/0029VbCVcLH2ZjCkxisZTd0l',
+    designer:  'https://emdesignsbb.com',
+  },
+};
